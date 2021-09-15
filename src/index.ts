26e5903054ffd7ec5e9f886b748dfa754f03c36e
Nilo Otaviano <nnilotaviano@gmail.com>
2021-09-15 17:29:26 -0300

    Export all consts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
-export {
-  BigintIsh,
-  ChainId,
-  TradeType,
-  Rounding,
-  MINIMUM_LIQUIDITY
-} from './constants'
+export * from './constants'
 
 export * from './errors'
 export * from './entities'
